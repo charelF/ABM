9673d356d4e9a444a8b6e477572aa397fe00aa2e
Snoeprol <mariovanrooij@hotmail.com>
2021-01-07 16:34:38 +0100

    simple svg to array of europe

---
+++ src/distribute_land.py
@@ -0,0 +1,6 @@
+
+
+def distribute_land(n, svg_name):
+    """
+    Function that divides the land of a given bitmap over n different chuncks
+    """
---
+++ src/test_1.py
@@ -0,0 +1,51 @@
+from mesa import Model
+from mesa.space import MultiGrid, SingleGrid
+from mesa import Agent
+import random
+
+class TestModel(Model):
+    def __init__(self, width, height):
+        self.height = height
+        self.width = width
+        
+        self.grid = SingleGrid(self.width, self.height, torus=True)
+        
+        self.n_agents = 0
+        self.agents = []
+
+    
+    def new_agent(self, agent_type, pos):
+        '''
+        Method that enables us to add agents of a given type.
+        '''
+        self.n_agents += 1
+        
+        # Create a new agent of the given type
+        new_agent = agent_type(self.n_agents, self, pos)
+        
+        # Place the agent on the grid
+        self.grid.place_agent(new_agent, pos)
+        
+        # And add the agent to the model so we can track it
+        self.agents.append(new_agent)
+        
+    def remove_agent(self, agent):
+        '''
+        Method that enables us to remove passed agents.
+        '''
+        self.n_agents -= 1
+        
+        # Remove agent from grid
+        self.grid.remove_agent(agent)
+        
+        # Remove agent from model
+        self.agents.remove(agent)
+        
+    def step(self):
+        '''
+        Method that steps every agent. 
+        
+        Prevents applying step on new agents by creating a local list.
+        '''
+        for agent in list(self.agents):
+            agent.step()
