f1de9c36633e8c27e9c9c3d932d252747b937506
Charel Felten <charel.felten@gmail.com>
2021-01-08 11:02:24 +0100

    separated agent and model

--- model/model.py
+++ model/model.py
@@ -4,50 +4,7 @@ from mesa.time import RandomActivation
 from mesa_geo.geoagent import GeoAgent, AgentCreator
 from mesa_geo import GeoSpace
 import random
-
-
-class SchellingAgent(GeoAgent):
-    """Schelling segregation agent."""
-
-    def __init__(self, unique_id, model, shape, agent_type=None):
-        """Create a new Schelling agent.
-
-        Args:
-            unique_id: Unique identifier for the agent.
-            agent_type: Indicator for the agent's type (minority=1, majority=0)
-        """
-        super().__init__(unique_id, model, shape)
-        self.atype = agent_type
-
-    def step(self):
-        """Advance agent one step."""
-        similar = 0
-        different = 0
-        neighbors = self.model.grid.get_neighbors(self)
-        if neighbors:
-            for neighbor in neighbors:
-                if neighbor.atype is None:
-                    continue
-                elif neighbor.atype == self.atype:
-                    similar += 1
-                else:
-                    different += 1
-
-        # If unhappy, move:
-        if similar < different:
-            # Select an empty region
-            empties = [a for a in self.model.grid.agents if a.atype is None]
-            # Switch atypes and add/remove from scheduler
-            new_region = random.choice(empties)
-            new_region.atype = self.atype
-            self.model.schedule.add(new_region)
-            self.atype = None
-            self.model.schedule.remove(self)
-        else:
-            self.model.happy += 1
-
-    def __repr__(self):
-        return "Agent " + str(self.unique_id)
+from agent import *
 
 
 class SchellingModel(Model):
